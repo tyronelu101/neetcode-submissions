9e6e834a4c3723953d22265629baa6118bb14f19
Ty <54049632+tyronelu101@users.noreply.github.com>
2026-06-02 23:20:44 -0500

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        results = [0] * len(nums)
+        
+        #Do two passes
+        #First pass we calculate the prefix and store in results
+        #Second pass we calculate the postfix at each position and multiply the value 
+        #with the prefix value stored in the results
+        prefix = 1
+        for i in range(len(nums)):
+            if i-1 == -1:
+                results[0] = prefix
+            else:
+                prefix *= nums[i-1]
+                results[i] = prefix
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) -1:
+                results[i] *= postfix
+            else:
+                postfix *= nums[i+1]
+                results[i] *= postfix
+
+
+        return results
