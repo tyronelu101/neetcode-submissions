5bc4e78e0d3863de66ed8feda04d46f0b87bb4bb
Ty <54049632+tyronelu101@users.noreply.github.com>
2026-06-16 22:21:10 -0500

    Add: longest-consecutive-sequence - submission-14

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        if len(nums) == 0 or len(nums) == 1:
+            return len(nums)
+
+        hash_set = set(nums)
+        longest_streak = 1
+
+        for num in nums:
+            current_streak = 1
+            # Check if start of a sequence
+            # It's the start if num-1 does not exist
+            if (num - 1) not in hash_set:
+                target = num + 1
+                # While target is in the hash_set
+                while target in hash_set:
+                    # Keep incrementing the target and checking hash_set
+                    target += 1
+                    current_streak += 1
+                if current_streak > longest_streak:
+                    longest_streak = current_streak
+
+        return longest_streak
